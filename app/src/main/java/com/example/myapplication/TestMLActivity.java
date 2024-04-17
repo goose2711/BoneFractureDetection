356189cd31a70c6518a4c5e5450718b4a17e3abd
goose_2711 <agastya.blr12@gmail.com>
2024-04-17 03:06:09 -0700

    made some changes to test ml file

--- app/src/main/java/com/example/myapplication/TestMLActivity.java
+++ app/src/main/java/com/example/myapplication/TestMLActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.ml.Resnet50ElbowFrac;
+
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
@@ -19,20 +24,28 @@ public class TestMLActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tf_lite);
-        try {
-            tflite = new Interpreter(loadModelFile());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+//        try {
+//            tflite = new Interpreter(loadModelFile());
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+         try {
+            Resnet50ElbowFrac model = Resnet50ElbowFrac.newInstance(context);
 
-    private MappedByteBuffer loadModelFile() throws IOException {
-        AssetFileDescriptor fileDescriptor = this.getAssets().openFd("model.tflite");
-        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileDescriptor.getStartOffset();
-        long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Resnet50ElbowFrac.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+
     }
 
     @Override
